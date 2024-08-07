3840a6145861795de13d315a080dbf7754da3d74
Aniket9977 <aniketvishwakarma459@gmail.com>
2024-08-08 00:28:38 +0530

    modified:   src/pipeline/predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import pandas as pd
-from src.exception import CustomError  
+from src.exception import CustomError
 from src.utils import load_object
 from src.logger import logging
 
@@ -9,28 +9,38 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path='artifact\model.pkl'
-            preprocessor_path='artifact\processor.pkl'
-            print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            model_path = 'artifact/model.pkl'
+            preprocessor_path = 'artifact/processor.pkl'
+            
+            # Debugging statements
+            print("Before Loading Model")
+            model = load_object(file_path=model_path)
+            print("Model Loaded Successfully")
+            
+            print("Before Loading Preprocessor")
+            preprocessor = load_object(file_path=preprocessor_path)
+            print("Preprocessor Loaded Successfully")
+            
+            # Transform the features
+            data_scaled = preprocessor.transform(features)
+            print("Features Transformed Successfully")
+            
+            # Make predictions
+            preds = model.predict(data_scaled)
+            print("Prediction Made Successfully")
             return preds
 
         except Exception as e:
-            raise CustomError(e , sys)
-
-
+            print(f"Error: {e}")
+            raise CustomError(e, sys)
 
 class CustomData:
     def __init__(self,
         gender: str,
         race_ethnicity: str,
-        parental_level_of_education,
+        parental_level_of_education: str,
         lunch: str,
         test_preparation_course: str,
         reading_score: int,
@@ -39,11 +49,11 @@ class CustomData:
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
+
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
@@ -55,9 +65,8 @@ class CustomData:
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
             }
-            logging.info ('Data mapping successfull')
+            logging.info('Data mapping successful')
             return pd.DataFrame(custom_data_input_dict)
-            
 
         except Exception as e:
             raise CustomError(e, sys)
